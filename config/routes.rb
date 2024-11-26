b5dd8bc86d6db7aed0e888586c1f4394322fde0c
EC2 Default User <ec2-user@ip-172-31-37-86.ap-northeast-1.compute.internal>
2024-11-26 23:38:53 +0000

    admin/sessionsコントローラー　パス記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  
+  namespace :admin do
+    get 'homes/top'
+  end
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
   devise_for :customers
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
