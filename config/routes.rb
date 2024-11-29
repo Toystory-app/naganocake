0fa525ff162aff0beb3750c19c8e8b72200798a2
EC2 Default User <ec2-user@ip-172-31-37-86.ap-northeast-1.compute.internal>
2024-11-29 03:23:07 +0000

    権限の設定追加 before_action/ストロングパラメータ/遷移先の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,21 @@
 Rails.application.routes.draw do
   
   namespace :admin do
+    #urlにadminが指定される
     get 'homes/top'
   end
-  devise_for :admins, controllers: {
+  devise_for :admins, skip: [:registraions, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
-  devise_for :customers
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: 'public/registrations',
+    sessions: 'public/sessions'
+  }
+  
+  scope module: :public do
+    #urlにpublicが指定されない
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+  end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
